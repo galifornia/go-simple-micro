26f955756ea26ee62f9c0aef61dcf86e38ec9eb1
galifornia <martinho87@gmail.com>
2022-07-17 00:16:29 +0200

    Kick off Broker service

---
+++ broker/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting Broker service in port %s\n", PORT)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", PORT),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
--- front-end/go.mod
+++ front-end/go.mod
@@ -1,3 +1,3 @@
-module frontend
+module github.com/galifornia/go-micro-frontend
 
 go 1.18
