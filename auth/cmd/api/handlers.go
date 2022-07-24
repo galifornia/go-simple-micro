165f907606a937158ab8dd00b2c54fa64c651a76
galifornia <martinho87@gmail.com>
2022-07-24 23:30:07 +0200

    [auth] Add /authenticate handler

--- auth/cmd/api/handlers.go
+++ auth/cmd/api/handlers.go
@@ -1,12 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
 func (app *Config) Auth(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
-		Error:   false,
-		Message: "Hit the authentication service!!",
+	var payload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
-	_ = app.writeJSON(w, http.StatusOK, payload)
+	err := app.readJSON(w, r, &payload)
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate the user
+	user, err := app.Models.User.GetByEmail(payload.Email)
+	if err != nil {
+		app.errorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	valid, err := user.PasswordMatches(payload.Password)
+	if err != nil || !valid {
+		app.errorJSON(w, errors.New("Invalid credentials"), http.StatusBadRequest)
+		return
+	}
+
+	response := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("User %s has been logged in", user.FirstName),
+		Data:    user,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, response)
 }
