b4a58c65d3cf98969f54afa7d402b9e3503d1637
galifornia <martinho87@gmail.com>
2022-07-25 00:03:47 +0200

    [broker]: handle & redirect submissions

--- broker/cmd/api/handlers.go
+++ broker/cmd/api/handlers.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 )
+
+type RequestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -11,4 +24,65 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var payload RequestPayload
+
+	err := app.readJSON(w, r, &payload)
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+
+	switch payload.Action {
+	case "auth":
+		app.Authenticate(w, payload.Auth)
+	default:
+		app.errorJSON(w, errors.New("Unknown action"))
+	}
+}
+
+func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload) {
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+	req, err := http.NewRequest("POST", "http://auth-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("Invalid credentials"))
+		return
+	} else if res.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("Something went wrong with Auth service"))
+		return
+	}
+
+	var response jsonResponse
+	err = json.NewDecoder(res.Body).Decode(&response)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if response.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Authenticated"
+	payload.Data = response.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+
 }
