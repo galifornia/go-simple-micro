8416ce6cace40bed07400b8367ffed42df65fc3d
galifornia <martinho87@gmail.com>
2022-08-03 18:58:20 +0200

    Add MongoDB deps & boilerplate for connecting to it

--- logger/cmd/api/main.go
+++ logger/cmd/api/main.go
@@ -1,13 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const (
+	WEB_PORT  = "80"
+	RPC_PORT  = "5001"
+	GRPC_PORT = "50001"
+	MONGO_URL = "mongodb://mongo:27017"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
 func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
 	for {
-		fmt.Println("Logger service")
+		log.Println("Logger service")
 		time.Sleep(8 * time.Second)
 	}
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(MONGO_URL)
+
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password"
+	})
+
+	conn, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return conn, nil
 }
