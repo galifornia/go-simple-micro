213062023eba3a4e67321fa0ac6bd89e10441125
galifornia <martinho87@gmail.com>
2022-08-06 19:36:00 +0200

    [broker] fix errors & warning + handler for communicating with Logger service

--- broker/cmd/api/handlers.go
+++ broker/cmd/api/handlers.go
@@ -8,13 +8,19 @@ import (
 )
 
 type RequestPayload struct {
-	Action string      `json:"action"`
-	Auth   AuthPayload `json:"auth,omitempty"`
+	Action string        `json:"action"`
+	Auth   AuthPayload   `json:"auth,omitempty"`
+	Logger LoggerPayload `json:"logger,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LoggerPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -36,13 +42,47 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 
 	switch payload.Action {
 	case "auth":
-		app.Authenticate(w, payload.Auth)
+		app.authenticate(w, payload.Auth)
+	case "logger":
+		app.log(w, payload.Logger)
 	default:
-		app.errorJSON(w, errors.New("Unknown action"))
+		app.errorJSON(w, errors.New("unknown action"))
 	}
 }
 
-func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload) {
+func (app *Config) log(w http.ResponseWriter, entry LoggerPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t") // In production shoulld be Marshal wo Indent
+	logServiceURL := "http://logger-service/log"       // !FIXME: read config from env
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 	req, err := http.NewRequest("POST", "http://auth-service/authenticate", bytes.NewBuffer(jsonData))
 	if err != nil {
@@ -59,10 +99,10 @@ func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload) {
 	defer res.Body.Close()
 
 	if res.StatusCode == http.StatusUnauthorized {
-		app.errorJSON(w, errors.New("Invalid credentials"))
+		app.errorJSON(w, errors.New("invalid credentials"))
 		return
 	} else if res.StatusCode != http.StatusAccepted {
-		app.errorJSON(w, errors.New("Something went wrong with Auth service"))
+		app.errorJSON(w, errors.New("something went wrong with Auth service"))
 		return
 	}
 
@@ -84,5 +124,4 @@ func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Data = response.Data
 
 	app.writeJSON(w, http.StatusAccepted, payload)
-
 }
