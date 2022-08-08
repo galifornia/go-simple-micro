448524252d9d7bb6524a28ce13d83618d362084f
galifornia <martinho87@gmail.com>
2022-08-08 16:28:25 +0200

    [mail] Add templates & create Mailer & add sendMail handler

--- mail/cmd/api/handlers.go
+++ mail/cmd/api/handlers.go
@@ -2,12 +2,38 @@ package main
 
 import "net/http"
 
-type JSONPayload struct {
+type mailMessage struct {
 	To      string `json:"to"`
+	From    string `json:"from"`
 	Subject string `json:"subject"`
-	Body    string `json:"body"`
+	Message string `json:"message"`
 }
 
 func (app *Config) sendMail(w http.ResponseWriter, r *http.Request) {
+	var requestPayload mailMessage
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Sent to" + requestPayload.To,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
